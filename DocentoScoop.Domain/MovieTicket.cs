c90f9f1161f060ae81c58b3f08acd7e2cd8cb8c9
Marc Mathijssen <maa.mathijssen@avans.nl>
2024-02-05 09:28:06 +0100

    Cleaning!

--- DocentoScoop.Domain/MovieTicket.cs
+++ DocentoScoop.Domain/MovieTicket.cs
@@ -1,35 +1,33 @@
-﻿
-using DocentoScoop.Domain;
+﻿namespace DocentoScoop.Domain;
 
 public class MovieTicket
+{
+    private int rowNr;
+    private int seatNr;
+    private bool isPremium;
+    private MovieScreening movieScreening;
+
+    public MovieTicket(MovieScreening movieScreening, int rowNr, int seatNr, bool isPremium)
     {
-        private int rowNr;
-        private int seatNr;
-        private bool isPremium;
-        private MovieScreening movieScreening;
-
-        public MovieTicket(MovieScreening movieScreening, int rowNr, int seatNr, bool isPremium)
-        {
-            this.movieScreening = movieScreening;
-            this.rowNr = rowNr;
-            this.seatNr = seatNr;
-            this.isPremium = isPremium;
-        }
-
-        public bool IsPremiumTicket()
-        {
-            return isPremium;
-        }
-
-        public decimal GetPrice()
-        {
-            return movieScreening.getPricePerSeat();
-        }
-
-        public DateTime GetScreeningDate()
-        {
-            return movieScreening.getDate();
-        }
-
+        this.movieScreening = movieScreening;
+        this.rowNr = rowNr;
+        this.seatNr = seatNr;
+        this.isPremium = isPremium;
     }
 
+    public bool IsPremiumTicket()
+    {
+        return isPremium;
+    }
+
+    public decimal GetPrice()
+    {
+        return movieScreening.getPricePerSeat();
+    }
+
+    public DateTime GetScreeningDate()
+    {
+        return movieScreening.getDate();
+    }
+
+    }
